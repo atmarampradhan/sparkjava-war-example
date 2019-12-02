f137f24aff5c087f1318f3540a57fe168272608e
atmarampradhan <46604279+atmarampradhan@users.noreply.github.com>
2019-12-02 15:32:00 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "<html><center><font size="15" color="blue"><B>Sky Devops Implemantation</B></font></center></html>");
+		get("/hello", (req, res) -> "<html><center><font size="45"><B>Sky Devops Implemantation</B></font></center></html>");
 	}
 }
